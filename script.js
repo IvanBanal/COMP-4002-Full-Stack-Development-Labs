96e1306a52b2de9035386eaefbdc020202bc354c
IvanBanal <ibanal@rrc.ca>
2026-09-09 18:56:33 -0500

    Adding all Audit employee names.

--- script.js
+++ script.js
@@ -3,13 +3,23 @@
  */
 // We will use objects.
 const departments = [
- {
-    name: "Administration",
-    employees: [
-        { firstName: "Zoë", lastName: "Robins" },
-        { firstname: "Madeleine", lastName: "Madden" }
-    ]
- },
+    {
+        name: "Administration",
+        employees: [
+            { firstName: "Zoë", lastName: "Robins" },
+            { firstname: "Madeleine", lastName: "Madden" }
+        ]
+    },
 
- 
+    {
+        name: "Audit",
+        employees: [
+            { firstName: "Josha", lastName: "Sadowski" },
+            { firstName: "Kate", lastName: "Fleetwood" }
+        ]
+    },
+
+    
+
+
 ];
