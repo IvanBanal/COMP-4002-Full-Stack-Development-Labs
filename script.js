b262feb6353b96ab1e8681b060460d10c2f1f3d1
IvanBanal <ibanal@rrc.ca>
2026-09-09 18:54:39 -0500

    Adding objects inside departments array. Also, adding Administration and all employee names under it.

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * NAME AND DEPARTMENT DATA 
+ */
+// We will use objects.
+const departments = [
+ {
+    name: "Administration",
+    employees: [
+        { firstName: "Zoë", lastName: "Robins" },
+        { firstname: "Madeleine", lastName: "Madden" }
+    ]
+ },
+
+ 
+];
