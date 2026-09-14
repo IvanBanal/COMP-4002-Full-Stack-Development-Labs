ae1fef9351890000e8b9b2b20b9f9c3d1d57ca13
IvanBanal <ibanal@rrc.ca>
2026-09-14 16:56:39 -0500

    Creating EmployeeDirectoryProps interface and EmployeeDirecotry function for EmployeeDirectory.tsx.

---
+++ Lab 1.2/src/components/EmployeeDirectory/EmployeeDirectory.tsx
@@ -0,0 +1,26 @@
+import type { Department } from "../../types/Employee";
+
+interface EmployeeDirectoryProps {
+    departments: Department[];
+}
+
+export function EmployeeDirectory({ departments }: EmployeeDirectoryProps) {
+    return (
+        <main>
+            {departments.map((department) => (
+                <section key={department.name}>
+                    <h2>{department.name}</h2>
+
+                    <ul>
+                        {department.employees.map((employee) => (
+                            <li key={`${employee.firstName}-${employee.lastName}`}>
+                                {employee.firstName} {employee.lastName}
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            ))}
+        </main>
+    )
+}
+
