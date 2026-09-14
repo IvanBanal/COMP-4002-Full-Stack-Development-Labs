22f2a3264a4af8f4686f4a1be751e1dcab65bdbd
IvanBanal <ibanal@rrc.ca>
2026-09-14 17:04:19 -0500

    Adding inline comments for sections different from Lab 1.1 to Lab 1.2.

--- Lab 1.2/src/components/EmployeeDirectory/EmployeeDirectory.tsx
+++ Lab 1.2/src/components/EmployeeDirectory/EmployeeDirectory.tsx
@@ -7,11 +7,14 @@ interface EmployeeDirectoryProps {
 export function EmployeeDirectory({ departments }: EmployeeDirectoryProps) {
     return (
         <main>
+            {/* For every department, create a <section>. */}
             {departments.map((department) => (
+                // Using key so that React uses it to identify items when rendering lists.
                 <section key={department.name}>
                     <h2>{department.name}</h2>
 
                     <ul>
+                        {/* For every employee in this department, create an <li>. */}
                         {department.employees.map((employee) => (
                             <li key={`${employee.firstName}-${employee.lastName}`}>
                                 {employee.firstName} {employee.lastName}
