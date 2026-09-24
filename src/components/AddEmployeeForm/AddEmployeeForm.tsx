5e7604c7b2aac2ae03bfa1ca15fa57d4f2d71bac
IvanBanal <ibanal@rrc.ca>
2026-09-24 11:48:07 -0500

    Adding useState functionality and logic to AddEmployeeForm.tsx.

--- src/components/AddEmployeeForm/AddEmployeeForm.tsx
+++ src/components/AddEmployeeForm/AddEmployeeForm.tsx
@@ -1,9 +1,32 @@
 import "./AddEmployeeForm.css"
+import { useState } from "react";
 
 export function AddEmployeeForm() {
+    const [firstName, setFirstName] = useState("");
+
     return (
         <form>
             <h2>Add Employee</h2>
+
+            <label>
+                First Name:
+                <input 
+                    type="text"
+                    value={firstName}
+                    /**
+                     * For reference:
+                     * onChange is a React event handler. Runs when the value in this input changes. 
+                     * (event) => is an arrow function. 
+                     * setFirstname is taking whatever the user currently typed and save it as firstnName.
+                     * event contains infomration about what happened.
+                     * target is the HTML element that caused the event, in this case the <input />
+                     * value is the current text inside the input HTML element. 
+                     * TLDR: setFirstName(event.target.value) gets the user's current input text and updates
+                     * the firstName state with that value. 
+                     */
+                    onChange={(event) => setFirstName(event.target.value)}
+                />
+            </label>
         </form>
     );
 }
